159e6243159d07ad24cd5d047d34414eb48079d7
Alain Schaller <SchallerAla@gmail.com>
2016-02-22 11:26:11 +0100

    never used view

--- resources/views/pages/register.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.default')
-
-@section('title', 'Page Title bla')
-
-@section('content')
-    <p>Please insert your personal data to create a new Account:</p>
-    @include('auth/register')
-@endsection
