84c23316ea118fd31863792541da4ffd9973e2dc
Alain Schaller <SchallerAla@gmail.com>
2016-02-23 12:43:07 +0100

    Multiple little things - Correct Welcome controller - Use column system for footer, home and about page

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,1 +1,3 @@
-&copy; Copyright 2015
+<div class="col-md-12">
+    &copy; Copyright 2015
+</div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,10 +3,19 @@
 @section('title', 'Page Title bla')
 
 @section('content')
-<h2>Welcome</h2>
-    <p>Hey {{ Auth::user()->first_name }}, welcome to our page! If this is your first time logged in,
-        you may go to the product section and register the bulb you bought. Afterwards you can check on
-        the measured values to get a better understanding of your room climate or gas protection...</p>
+
+    <div class="col-md-6 col-md-offset-3">
+        <h2>Welcome</h2>
+        <p>
+            Hey {{ Auth::user()->first_name }},
+        </p>
+        <p>
+            Welcome to our page! If this is your first time logged in,
+            you may go to the product section and register the bulb you bought. Afterwards you can check on
+            the measured values to get a better understanding of your room climate or gas protection...
+        </p>
+    </div>
+
 	
 	
 @endsection
